31ec41b956534e1a598c8ecadf9b7c4f277d65a1
Adijair <adijairp05@gmail.com>
2024-01-11 13:00:53 -0300

    execícios do capítulo 2

---
+++ Preparação do ambiente/atividades/exercício2,5.py
@@ -0,0 +1,7 @@
+var1 = int(input("Primeira variável: "))
+var2 = int(input("Segunda variável: "))
+var3 = int(input("Terceira variável: "))
+
+resutado = var1 + var2 + var3
+
+print("A soma das variáves é: ", resutado)
