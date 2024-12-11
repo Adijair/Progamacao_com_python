3e27d1baf3a53247614d22c6982dc6dd35778885
Adijair <adijairp05@gmail.com>
2024-12-11 14:23:39 -0300

    exercicios

--- Repetições/test.py
+++ Repetições/test.py
@@ -13,10 +13,3 @@ while x <= fim:
     print(x)
     x = x + 1
     """
-
-fim = int(input("Digite o utimo numoro a inprimir: "))
-x = 1
-while x <= fim:
-    if x % 2 == 0:
-        print(x)
-        x = x + 1
---
+++ Repetições/test02.py
@@ -0,0 +1,16 @@
+#maneiras de sempre deixa o numero par
+
+"""
+fim = int(input("Digite o utimo numoro a inprimir: "))
+x = 0
+while x <= fim:
+    if x % 2 == 0:
+        print(x)
+    x = x + 1
+"""
+
+fim = int(input("Digite o utimo numoro a inprimir: "))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 2
